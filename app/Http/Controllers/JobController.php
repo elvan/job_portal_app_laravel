b429a2643b0aa15c8086ba0bfd2d938963740e0a
Elvan Hidayat <elvanhd@gmail.com>
2023-06-20 20:04:30 +0700

    displaying other employer jobs

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -50,7 +50,7 @@ class JobController extends Controller
     {
         return view(
             'job.show',
-            ['job' => $job->load('employer')]
+            ['job' => $job->load('employer.jobs')]
         );
     }
 
