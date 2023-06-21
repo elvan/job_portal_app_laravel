4b1803fb2d1ca8892d4ecca5ef2fd9e78d54ba94
Elvan Hidayat <elvanhd@gmail.com>
2023-06-21 19:31:13 +0700

    the logic for applying for jobs

--- app/Models/JobApplication.php
+++ app/Models/JobApplication.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Job;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,6 +11,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class JobApplication extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['expected_salary', 'user_id', 'job_id'];
 
     public function job(): BelongsTo
     {
